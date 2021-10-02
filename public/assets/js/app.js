5b8f7728a4c72903efaa7cc2d52deb9a765fd917
Zaya1101 <isaiah_landicho@hotmail.com>
2021-10-02 14:25:35 +1000

    changing camera constraints

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,48 +1,41 @@
 //JS for the camera function. Also saves it to the server database.
 function setup() {
-  noCanvas();
 
   let constraints = {
     video: {
-      mandatory: {
-        minWidth: 375,
-        minHeight: 812
+        width: 375,
+        height: 812,
+        facingMode: {
+          exact: "environment"
       },
     },
   audio: false
   };
 
-  const video = createCapture(constraints, function(stream) {
-    console.log(stream);
+  let sketch = function(p) {
+    p.setup = function(){
+      const video = p.createCapture(constraints, function(stream) {
+        console.log(stream);
+      });
+    }
+  };
+  new p5(sketch, window.document.getElementById('container'));
+
+  let lat, lon;
+  const button = document.getElementById('submit');
+  button.addEventListener('click', async event => {
+    video.loadPixels();
+    const image64 = video.canvas.toDataURL();
+    const data = { lat, lon, image64};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    const response = await fetch('/api', options); 
+    const json = await response.json();
+    console.log(json);
   });
-
-    let lat, lon;
-    const button = document.getElementById('submit');
-    button.addEventListener('click', async event => {
-        video.loadPixels();
-        const image64 = video.canvas.toDataURL();
-        const data = { lat, lon, image64};
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        };
-        const response = await fetch('/api', options); 
-        const json = await response.json();
-        console.log(json);
-    });
-
-    if ('geolocation' in navigator) {
-    console.log('geolocation available');
-        navigator.geolocation.getCurrentPosition(async position => {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        document.getElementById('latitude').textContent = lat;
-        document.getElementById('longitude').textContent = lon;
-    });
-} else {
-    console.log('geolocation not available');
-}
 }
--- public/assets/js/logs.js
+++ public/assets/js/logs.js
@@ -6,18 +6,16 @@ async function getData() {
 
     for (item of data) {
         const root = document.createElement('div');
-        const geo = document.createElement('div');
         const date = document.createElement('div');
         const image = document.createElement('img');
 
         root.style.cssText = 'padding: 10px;';
         
-        geo.textContent = `${item.lat}°, ${item.lon}°`;
         const dateString = new Date(item.timestamp).toLocaleString();
         date.textContent = dateString;
         image.src = item.image64;
 
-        root.append(geo, date, image);
+        root.append(date, image);
         document.body.append(root);
     }
     console.log(data);
