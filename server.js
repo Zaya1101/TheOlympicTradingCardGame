db0e98dbda009da2a39d905329d436582359dc19
Zaya1101 <isaiah_landicho@hotmail.com>
2021-10-07 12:41:41 +1000

    possible fix

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const Datastore = require('nedb');
 const app = express();
 
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Starting server at ${port}`)
@@ -33,3 +35,4 @@ app.post('/api', (request, response) => {
     database.insert(data);
     response.json(data);
 });
+
