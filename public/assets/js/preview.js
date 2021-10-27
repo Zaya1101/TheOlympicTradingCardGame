fa79d7bea81407188897060d564abd668827cfe4
Zaya1101 <isaiah_landicho@hotmail.com>
2021-10-27 12:16:49 +1000

    please work

--- public/assets/js/preview.js
+++ public/assets/js/preview.js
@@ -68,27 +68,7 @@ function setup() {
     //Draggable objects
     //Need to find a way to append this to the div container so HTML2Canvas can snapshot it.
     //Make it work for multiple objects
-    function drag_start(event) {
-        var style = window.getComputedStyle(event.target, null);
-        event.dataTransfer.setData("text/plain",
-        (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-    } 
-    function drag_over(event) { 
-        event.preventDefault(); 
-        return false; 
-    } 
-    function drop(event) { 
-        var offset = event.dataTransfer.getData("text/plain").split(',');
-        var dm = document.getElementById('dragme');
-        dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-        dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-        event.preventDefault();
-        return false;
-    } 
-    var dm = document.getElementById('dragme'); 
-    dm.addEventListener('dragstart',drag_start,false); 
-    document.body.addEventListener('dragover',drag_over,false); 
-    document.body.addEventListener('drop',drop,false); 
+
 
     
 
@@ -132,4 +112,33 @@ function setup() {
         cardBorder.style.cssText = "border-image: linear-gradient(45deg,#D82924, #981B1D 55%) 1;"
     });
 
+
+    //Eventually fix this for multiple objects bruh
+    function drag_start(event) {
+        var style = window.getComputedStyle(event.target, null);
+        event.dataTransfer.setData("text/plain", (parseInt(style.getPropertyValue("left"), 10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - event.clientY) + ',' + event.target.getAttribute('data-item'));
+    }
+    
+    function drag_over(event) {
+        event.preventDefault();
+        return false;
+    }
+    
+    function drop(event) {
+        var offset = event.dataTransfer.getData("text/plain").split(',');
+        var dm = document.getElementsByClassName('dragme');
+        var dropContainer = document.getElementById('cardContainer');
+        dm[parseInt(offset[2])].style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
+        dm[parseInt(offset[2])].style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
+        dropContainer.appendChild(dm[parseInt(offset[2])]); 
+        event.preventDefault();
+        return false;
+    }
+
+    var dm = document.getElementsByClassName('dragme');
+    for (var i = 0; i < dm.length; i++) {
+        dm[i].addEventListener('dragstart', drag_start, false);
+        document.body.addEventListener('dragover', drag_over, false);
+        document.body.addEventListener('drop', drop, false);
+      }
 }
