3faaf6d26a17786bfb2026251bceeea73cf31065
Zaya1101 <isaiah_landicho@hotmail.com>
2022-02-09 13:36:31 +1000

    final

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 //JS for the server
 
-//const { request, response, Router } = require('express');
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const cardImage = require('./models/tradingCardImages');
 const rawImage = require('./models/rawImages');
 const backgroundImage = require('./models/backgroundImages');
-//const Datastore = require('nedb');
-//const multer = require('multer');
-//const upload = multer({ dest: 'uploads/' });
 const app = express();
 
 const dbURI = 'mongodb+srv://administrator:Isaiah01@images.5xl03.mongodb.net/card-images?retryWrites=true&w=majority';
@@ -21,9 +17,6 @@ mongoose.connect(dbURI)
 app.use(express.static('public'));
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({extended: false}));
-
-//const database = new Datastore('database.db');
-//database.loadDatabase();
 
 app.post('/api', (request, response) => {
     const data = request.body;
